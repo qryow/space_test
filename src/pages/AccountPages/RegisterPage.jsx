2589cdaa59d21a2f52b4fb0ded64ecee16341cff
qryow <elmar20061602@gmail.com>
2023-10-06 22:03:52 +0600

    login add

--- src/pages/AccountPages/RegisterPage.jsx
+++ src/pages/AccountPages/RegisterPage.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import Register from '../../components/Account/Register'
+import MainNavbar from '../../components/Main/MainNavbar'
+import style from './Account.module.css'
+
+import registerFon from '../../img/register.png'
 
 const RegisterPage = () => {
   return (
-    <Register />
+    <>
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <MainNavbar />
+          <div className={style.block__wrapper}>
+            <div className={style.block}>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+    //<Register />
   )
 }
 
