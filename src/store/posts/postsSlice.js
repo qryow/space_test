8d37ec03ba73d4ac32f15d0812088c008f3ed3e7
ada1533 <adakhanbelekov@gmail.com>
2023-11-23 22:38:41 +0600

    profile

--- src/store/posts/postsSlice.js
+++ src/store/posts/postsSlice.js
@@ -1,37 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getPosts } from "./postsActions";
+import { getPosts } from "./PostsActions";
 
-const postsSlice = createSlice({
-    name: 'posts',
-    initialState: {
-        posts: [],
-        loading: false,
-        onePost: null,
+const PostsSlice = createSlice({
+  name: "posts",
+  initialState: {
+    posts: [],
+    loading: false,
+    onePost: null,
+  },
+  reducers: {
+    clearOnePostState: (state) => {
+      state.onePost = null;
     },
-    reducers: {
-        clearOnePostState: (state) => {
-            state.onePost = null;
-        }
-    },
-    extraReducers: (builder) => {
-        builder
-        .addCase(getPosts.pending, (state) => {
-            state.loading = true;
-        })
-        .addCase(getPosts.fulfilled, (state, action) => {
-            state.loading = false;
-            state.posts = action.payload;
-            console.log(action.payload);
-        })
-        .addCase(getPosts.rejected, (state) => {
-            state.loading = false;
-        })
-        // .addCase(getPosts.fulfilled, (_, action) => {
-        //     action.payload.navigate('/profile');
-        // })
-    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPosts.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getPosts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.posts = action.payload;
+        console.log(action.payload);
+      })
+      .addCase(getPosts.rejected, (state) => {
+        state.loading = false;
+      });
+    // .addCase(getPosts.fulfilled, (_, action) => {
+    //     action.payload.navigate('/profile');
+    // })
+  },
+});
 
-})
-
-export const { clearOneProjectState } = postsSlice.actions;
-export default postsSlice.reducer;
+export const { clearOneProjectState } = PostsSlice.actions;
+export default PostsSlice.reducer;
